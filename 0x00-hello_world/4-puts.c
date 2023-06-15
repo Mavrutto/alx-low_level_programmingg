43f848ad50fe0dd596c4ca74375c88bcfc345d0d
Mavrutto <maverick1999only@gmail.com>
2023-06-15 14:19:15 -0700

    Task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * main- A program that is able to use the function puts
+ * the program should end with the value 0
+ */
 int main(void) 
 {	
 puts("Programming is like building a multilingual puzzle");
