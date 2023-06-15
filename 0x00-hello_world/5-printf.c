bdf86115f6fda4bd330bb26d86d14558720ccbba
Mavrutto <maverick1999only@gmail.com>
2023-06-15 15:14:23 -0700

    Task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - A program that is able to use the function printf
+* return: 0
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
