5c7b3fe2ee32c843b4c2d36d7256903192c61beb
Mavrutto <maverick1999only@gmail.com>
2023-06-15 15:32:34 -0700

    Task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - A program that is able to use the function puts
- * return: 0 (success)
- */
-int main(void) 
+* main - A program that is able to use the function puts
+* return: 0 (success)
+*/
+
+int main(void)
 {
-puts ("\"Programming is like building a multilingual puzzle");
-return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
-	
