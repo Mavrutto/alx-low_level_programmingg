c1ff21ab630873af5fe6a1ffb2c015a62d0fa30c
Mavrutto <maverick1999only@gmail.com>
2023-06-19 15:38:06 -0700

    TAsk7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description -prints lowercase
+ *Return: return 0(Success)
+*/
+int main(void)
+{
+char c;
+c = 'r';
+while
+(c >= 'a') {
+pitchar(c);
+c--;
+}
+putchar('\n');
+return (0);
+}
