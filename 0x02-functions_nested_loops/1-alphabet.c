d68a8cf542306cc5d2cf887b670a5a358f7f7469
Mavrutto <maverick1999only@gmail.com>
2023-06-21 09:43:37 -0700

    Task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * description - lowercase
+ * return: always 0(Success)
+*/
+void print_alphabet(void);
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchar("/n");
+return (0);
+}
