c27d31e72b1949692425f91b22d03838866bda1d
Mavrutto <maverick1999only@gmail.com>
2023-06-23 16:38:47 -0700

    Task2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *mul - multiplying a and b
+ *@a - first interger
+ *@b - second interger
+ *return: 0(Success)
+*/
+int mul(int a, int b)
+{
+int result = a * b;
+return (result);
+}
