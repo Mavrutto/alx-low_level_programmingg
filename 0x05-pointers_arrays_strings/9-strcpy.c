f458c9c982c82734fbdb084fa5a30b9aa7148e9d
Mavrutto <maverick1999only@gmail.com>
2023-06-27 17:41:18 -0700

    Task101

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * *_strcpy - copies strings pointed by the src
+ * strcpy - copies strings pointed by the src
  * @dest: char type string
  * @src: char type string
  * description: copy the string pointed to
