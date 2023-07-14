596cf1ae7ee0d42c1347f29791e0867c853f7761
Mavrutto <maverick1999only@gmail.com>
2023-07-13 17:16:30 -0700

    Task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description - using puts to print
+ * Return: 0(always)
+ */
+int main(void)
+{
+char Mystring[] = "Programming is like building a multilingual puzzle";
+puts(Mystring);
+return (0);
+}
