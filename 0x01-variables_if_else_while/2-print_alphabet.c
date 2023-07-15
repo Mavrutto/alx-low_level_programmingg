2052e3013812ca131f23e4a66a6bbced41b779af
Mavrutto <maverick1999only@gmail.com>
2023-07-15 07:35:25 -0700

    Task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: lowercase
+ * Return: (0)
+ */
+int main (void)
+{
+int a = 97;
+while (a <= 122)
+{
+putchar(a++);
+}
+putchar('\n');
+return (0);
+}
