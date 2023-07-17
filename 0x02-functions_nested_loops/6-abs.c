d3b3f94c16824482ed4e440d7a2fcb97b6884e31
Mavrutto <maverick1999only@gmail.com>
2023-07-17 06:34:57 -0700

    Task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _abs - entry
+ * @i: variable
+ * Return: 0 and 1
+ *
+ *
+ */
+int _abs(int i)
+{
+if (i >= 0)
+{
+	return (i);
+}
+else
+{
+	return (i * -1);
+}
+}
