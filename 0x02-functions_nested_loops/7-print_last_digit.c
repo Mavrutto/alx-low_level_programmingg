e00fac5bc2ecb3a466ba82c8c8587f7441c827f9
Mavrutto <maverick1999only@gmail.com>
2023-07-17 22:39:41 -0700

    Task7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,14 +10,14 @@ int print_last_digit(int n)
 {
 int m;
 m = n % 10;
-if ( m < 0)
+if (m < 0)
 {
-	_putchar(-m + 48);
-        return (-m);
+_putchar(-m + 48);
+return (-m);
 }
 else
 {
-	_putchar(m + 48);
-	return (m);
+_putchar(m + 48);
+return (m);
 }
 }
