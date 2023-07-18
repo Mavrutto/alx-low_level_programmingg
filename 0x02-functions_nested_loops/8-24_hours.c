fd50533db0a803c24c74a26a59ea9a4f26692b39
Mavrutto <maverick1999only@gmail.com>
2023-07-17 21:58:17 -0700

    Task8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -17,13 +17,13 @@ for (a = 0; a <= 2; a++)
 			{
 				for (d = 0; d <= 9; d++)
 				{
-					 _putchar(a + '0');
+					 _putchar('0' + a);
                                          _putchar(b + '0');
                                          _putchar(58);
                                          _putchar(c + '0');
                                          _putchar(d + '0');
                                          _putchar('\n');
-				}
+			}
 			}
 		}
 	}
